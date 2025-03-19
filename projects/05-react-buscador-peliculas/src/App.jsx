c2818315c14ce8cf9e4357245b05ade3262599a3
Mario <mpucheu126@gmail.com>
2025-03-19 12:11:58 -0400

    Initializing react-buscador-peliculas

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import responseMovies from "./mocks/with-results.json";
+import withoutResults from "./mocks/no-results.json";
+import { Movies } from "./components/movies";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function useMovies () {
+  const movies = responseMovies.Search;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const mappedMovies = movies.map((movie) => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    poster: movie.Poster,
+  }));
+
+  return {movies: mappedMovies};
 }
 
-export default App
+function App() {
+  const { movies: mappedMovies } = useMovies();
+
+
+  return (
+    <div className="page">
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form action="" className="form">
+          <input placeholder="Nombre de una pelicula..." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+
+      <main>
+        <Movies movies={mappedMovies} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
