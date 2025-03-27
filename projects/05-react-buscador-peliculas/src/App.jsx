c654666561b411a22b32895d45e3125b11bcc8a6
Mario <mpucheu126@gmail.com>
2025-03-27 17:51:03 -0400

    finishing react-buscador-peliculas with sort by title and autoseach with debounce

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,38 +1,92 @@
 import "./App.css";
-import responseMovies from "./mocks/with-results.json";
-import withoutResults from "./mocks/no-results.json";
-import { Movies } from "./components/movies";
+import { useEffect, useRef, useState, useCallback } from "react";
+import { Movies } from "./components/Movies";
+import { useMovies } from "./hooks/useMovies";
+import debounce from "just-debounce-it";
 
-export function useMovies () {
-  const movies = responseMovies.Search;
+function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
 
-  const mappedMovies = movies.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster,
-  }));
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
 
-  return {movies: mappedMovies};
+    if (search == "") {
+      setError("No se puede buscar si el campo esta vacio");
+      return;
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError("No puede escribir solo numeros");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
 }
 
 function App() {
-  const { movies: mappedMovies } = useMovies();
+  const [sort, setSort] = useState(false);
 
+  const { search, updateSearch, error } = useSearch();
+  const { movies, loading, getMovies } = useMovies({ search, sort });
+
+  const debouncedGetMovies = useCallback(
+    debounce(search => {
+      console.log("search", search);
+      getMovies({ search });
+    }, 500),
+    [getMovies]
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getMovies({ search });
+  };
+
+  const handleSort = () => {
+    setSort(!sort);
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+    debouncedGetMovies(newSearch);
+  };
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de peliculas</h1>
-        <form action="" className="form">
-          <input placeholder="Nombre de una pelicula..." />
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: "1px solid transparent",
+              borderColor: error ? "red" : "transparent",
+            }}
+            onChange={handleChange}
+            value={search}
+            name="query"
+            placeholder="Nombre de una pelicula..."
+          />
+          <input type="checkbox" onChange={handleSort} checked={sort} />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
-      <main>
-        <Movies movies={mappedMovies} />
-      </main>
+      <main>{loading ? <p>cargando...</p> : <Movies movies={movies} />}</main>
     </div>
   );
 }
